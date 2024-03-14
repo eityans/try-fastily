c4d30f76339444aaaa3e7b28d5d9db66d6d56560
eityans <eityans@gmail.com>
2024-03-14 12:04:28 +0900

    define GET /identicon/:x

--- routes/root.js
+++ routes/root.js
@@ -1,7 +1,13 @@
-'use strict'
+"use strict";
 
 module.exports = async function (fastify, opts) {
-  fastify.get('/', async function (request, reply) {
-    return { root: true }
-  })
-}
+  fastify.get("/", async function (request, reply) {
+    reply.send("Hello World!");
+    //return { root: true }
+  });
+
+  fastify.get("/identicon/:x", async (request, reply) => {
+    const { x } = request.params;
+    reply.send(`x is ${x}`);
+  });
+};

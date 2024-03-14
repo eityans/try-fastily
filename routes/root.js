fc898377ab9a2b225eea800d19024f6418324548
eityans <eityans@gmail.com>
2024-03-14 12:08:07 +0900

    comment

--- routes/root.js
+++ routes/root.js
@@ -6,6 +6,7 @@ module.exports = async function (fastify, opts) {
     //return { root: true }
   });
 
+  // npm run devして、http://127.0.0.1:3000/identicon/aaa
   fastify.get("/identicon/:x", async (request, reply) => {
     const { x } = request.params;
     reply.send(`x is ${x}`);
